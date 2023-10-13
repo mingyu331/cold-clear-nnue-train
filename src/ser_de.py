1d07e279bef85e587611226c7924fd5e073c6e2e
muse918 <muse918911@gmail.com>
2023-10-13 13:32:24 +0900

    asdf

--- src/ser_de.py
+++ src/ser_de.py
@@ -1,1 +1,16 @@
 # serialization and deserialization of training files and nnue files
+import json
+def load_train(path):
+    with open(path) as f:
+        train_data = json.load(f)
+    return train_data
+
+def edit_data(path):
+    with open(path, 'r') as f:
+        n = f.read()
+        with open(path + '.edited.txt', 'w') as f2:
+            f2.write('['+n[:-1].replace('\n', ', \n')+']')
+
+edit_data('./train_data/out_high.txt')
+data = load_train('./train_data/out_high.txt.edited.txt')
+print(data)
